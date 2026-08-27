bc97b7c88234378b43e8e3f1a942d6d127161ffa
steddiK <steddi.andria@gmail.com>
2026-08-27 11:39:48 +0300

    feat: ontology

--- backend/app/models/reponse.py
+++ backend/app/models/reponse.py
@@ -5,13 +5,16 @@ from datetime import datetime
 from sqlalchemy import Column, String, Boolean, DateTime, Enum, Integer, ForeignKey, func, relationship
 from app.core.database import Base
 
-class Message(Base):
-    __tablename__ = "messages"
+class Reponse(Base):
+    __tablename__= "reponses"
 
     id = Column(Integer, primary_key=True, index=True)
     session_id = Column(Integer, ForeignKey("sessions.id"), nullable=False)
-    role = Column(String, nullable=False)  # user, assistant
+    message_id = Column(Integer, ForeignKey("message.id"), nullable=False)
+    trace_id = Column(Integer, ForeignKey("trace.id"), nullable=False)
     contenu = Column(String, nullable=False)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
 
-    session = relationship("SessionConversation", back_populates="messages")
+    session = relationship("SessionConversation", back_populates="reponses")
+    message = relationship("Message", back_populates="reponse")
+    trace = relationship("Trace", back_populates="reponse")
